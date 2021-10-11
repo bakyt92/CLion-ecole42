1d6bc19627ff08b460e81298fcfef18b0bca768c
Ultron Fitzhugh <ufitzhug@un-a1.kzn.21-school.ru>
2021-10-12 01:56:51 +0300

    Lion12102021_01

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,31 +1,35 @@
-//#include <stdio.h>
-//#include <unistd.h>
-//#include <stdlib.h>
-//
-//void ft_bzero(void *s, size_t n)
-//{
-//    unsigned char *l;
-//    l = (unsigned char*)s;
-//    size_t x;
-//    x = 0;
-//    while (x < n && *l != '\0')
-//    {
-//        *l = 0;
-//        l++;
-//        x++;
-//    }
-//}
-//
-//int main (void)
-//{
-//    void *s1 = "He5398ll423no";
-//
-//    size_t n;
-//    n = 7;
-//
-//    printf("Результат1: %s", s1);
-//    printf("\n Newline");
-//    ft_bzero(s1, n);
-//    printf("Результат2: %s", s1);
-//    return(0);
-//}
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ft_bzero(void *s, size_t n)
+{
+    unsigned char *l;
+    l = (unsigned char*)s;
+    size_t x;
+    x = 0;
+    while (x < n)
+    {
+        *l = '\0';
+        l++;
+        x++;
+    }
+}
+
+int main (void)
+{
+    char s1[] = "He5398ll423no";
+    char s2[] = "He5398ll423no";
+
+    size_t n;
+    n = 20;
+
+    printf("\n *****FT_BZERO****** \n");
+    printf("Результат ft_bzero1: %s", s1);
+    printf("\n  \n");
+    ft_bzero(s1, n);
+    printf("Результат ft_bzero2: %s", s1);
+    printf("\n");
+    return(0);
+}
