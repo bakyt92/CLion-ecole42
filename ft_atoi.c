b3d923696fa42908a0e7ac142157a9b1a52c8ded
ufitzhug <ufitzhug@ge-f4.kzn.21-school.ru>
2021-10-17 02:12:30 +0300

    17102021_03

---
+++ ft_atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ft_atoi(const char *str)
+{
+    long i;
+    long res;
+    res = 0;
+    int znak;
+    i = 0;
+
+    while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+        ++i;
+    if (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+            znak = -1;
+    ++i;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        res = res * 10 + str[i] - '0';
+        ++i;
+    }
+    return (res*znak);
+}
+
+int main(void)
+{
+    printf("Result: %d\n", ft_atoi("\t-245"));
+    return (0);
+}
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-
-char *ft_strnstr(char *first, const char *find, size_t len)
-{
-    size_t i = 0;
-    size_t j;
-    while (first[i] != '\0' && i < len)
-    {
-        j = 0;
-        while (find[j] != '\0' && first[i+j] == find[j] && (i + j) < len)
-            ++j;
-        if (find[j] == '\0')
-            return ((char *) first + i);
-        ++i;
-    }
-    return (NULL);
-}
-
-int main (void)
-{
-    size_t len;
-    char s1[] = "asdfghjc453";;
-    char s2[] = "df";
-    len = 10;
-    printf("Результат: %s", ft_strnstr(s1, s2, len));
-    return(0);
-}
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//char *ft_strnstr(char *first, const char *find, size_t len)
+//{
+//    size_t i = 0;
+//    size_t j;
+//    while (first[i] != '\0' && i < len)
+//    {
+//        j = 0;
+//        while (find[j] != '\0' && first[i+j] == find[j] && (i + j) < len)
+//            ++j;
+//        if (find[j] == '\0')
+//            return ((char *) first + i);
+//        ++i;
+//    }
+//    return (NULL);
+//}
+//
+//int main (void)
+//{
+//    size_t len;
+//    char s1[] = "asdfghjc453";;
+//    char s2[] = "df";
+//    len = 10;
+//    printf("Результат: %s", ft_strnstr(s1, s2, len));
+//    return(0);
+//}
