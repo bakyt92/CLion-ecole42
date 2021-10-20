2d83f39fb46372a8b1a08a8824debe753f469040
ufitzhug <ufitzhug@ge-f4.kzn.21-school.ru>
2021-10-21 01:57:11 +0300

    20102021_06

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,5 +1,7 @@
 #include "libft.h"
 
+// эта функция находит первый символ, который необходимо копировать (т.е.
+// символ, который не входит в set). Ищет с начала
 static size_t ft_begin(char const *s1, char const *set)
 {
 	size_t i;
@@ -20,7 +22,8 @@ static size_t ft_begin(char const *s1, char const *set)
 	}
 	return ((int)(void *)0);
 }
-
+// эта функция находит последний символ, который необходимо копировать в
+// новую строку (т.е. символ, который не входит в set. Ищет с конца
 static size_t ft_end(char const *s1, char const *set)
 {
 	size_t i;
@@ -43,6 +46,8 @@ static size_t ft_end(char const *s1, char const *set)
 	return (i);
 }
 
+// сама функция трим.
+
 char *ft_strtrim(char const *s1, char const *set)
 {
 	size_t begin;
@@ -50,14 +55,21 @@ char *ft_strtrim(char const *s1, char const *set)
 	size_t i;
 	char *s_new;
 
+// проверяем, что строки s1 / set не пустые
 	if (!s1 || !set)
 		return (NULL);
+// если функция ft_begin / ft_end возвращает NULL - то и основная функция
+// тоже возвращает NULL. В ином случае функция возвращает индекс первого или
+// последнего символа для копирования в новую строку
 	if (!ft_begin(s1, set))
 		return (NULL);
 	else begin = ft_begin(s1, set);
+	// я проверял на дебаггере, вроде бы ошибка именно в ft_end
 	if (!ft_end(s1, set))
 		return (NULL);
 	else end = ft_end(s1, set);
+
+// выделяем память, проверяем что выделили
 	s_new = (char *) malloc(sizeof(char) * (end - begin + 1));
 	if (!s_new)
 		return (s_new);
